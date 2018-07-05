2cd85f2ca82e00769e9aed1e43ef0b590a839906
AlexRohwer <alex.rohwer.ia@gmail.com>
2018-07-05 14:33:38 -0500

    project upload

---
+++ src/wordsimilarity/IndexCommand.java
@@ -0,0 +1,35 @@
+package wordsimilarity;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import opennlp.tools.stemmer.*;
+
+public class IndexCommand {
+
+    ArrayList<ArrayList> output;
+    File filename;
+
+    public IndexCommand(String filename) {
+        this.output = new ArrayList();
+        this.filename = new File(filename);
+    }
+
+    public void reader() throws FileNotFoundException {
+        Scanner fileScan = new Scanner(filename).useDelimiter("[!?.]+");
+        PorterStemmer stemmer = new PorterStemmer();
+        while (fileScan.hasNext()) {
+            String[] sent = fileScan.next().replaceAll("\\r\\n|\\r|\\n", " ").replaceAll("[^-A-Za-zÀ-ÿ0-9' ]", "").toLowerCase().split("\\s+");
+            ArrayList<String> additive = new ArrayList();
+            for (int i = 0; i < sent.length; i++) {
+                if (!((new Scanner(new File("stopwords.txt")).useDelimiter("\\Z").next()).contains(sent[i]))) {
+                    additive.add(stemmer.stem(sent[i]));
+                }
+            }
+            if (!additive.isEmpty()) {
+                output.add(additive);
+            }
+        }
+    }
+}
